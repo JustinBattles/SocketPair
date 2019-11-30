3bf956e857aad3f36ccea3b40450ceb279997f62
Justin Battles <jbattles393@gmail.com>
2019-11-29 22:09:39 -0500

    Update SocketPair.c

--- SocketPair.c
+++ SocketPair.c
@@ -10,17 +10,7 @@
 #include <string.h>
 
 #define K 1024
-/**
- * Create a parent function that is given the childs process id and the two
- * socketpair descriptors, then performs the following tasks:
- * 0) closes the unneeded side of the socketpair:
- * 1) Reads from stdin until EOF:
- *  a) Writes the data from stdin to the socket.
- *  b) Waits for the child to respond (i.e. reads from the socket)
- *  c) Writes the response to the screen (stdout)
- * 2) Closes the socket when EOF on the terminal is reached.
- * 3) Waits for the child to complete.
- */
+
 void parent(pid_t pid, int sv[2])
 {
   close(sv[0]);
